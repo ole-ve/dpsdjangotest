c8e035f2c390b250c9a25e1cdbe4d99e47dc0ee0
davidp <david.pop@tum.de>
2024-03-05 17:13:13 +0100

    sentiment analysis on full article

--- processing/app.py
+++ processing/app.py
@@ -8,18 +8,26 @@ from matplotlib import pyplot as plt
 from matplotlib import dates as mdates
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-news_file_path = 'data/queried_news.json'
-#news_file_path = 'data/data.json'
+#news_file_path = 'data/queried_news.json'
+news_file_path = 'data/data.json'
 news_data = pd.read_json(news_file_path)
 
-# gather news headlines to pass to the model
+# gather news headlines to pass to the model --- HEADLINE ONLY
 titles_list = news_data['title'].tolist()
+
+# gather news headlines & full article --- HEADLINE+FULLARTICLE
+news_data['full_article'] = news_data['title'] + " " + news_data['fullArticle']
+articles_list = news_data['full_article'].tolist()
+
+
 
 # load tokenizer & model
 tokenizer = AutoTokenizer.from_pretrained("ProsusAI/finbert")
 model = AutoModelForSequenceClassification.from_pretrained("ProsusAI/finbert")
 
-inputs = tokenizer(titles_list, padding=True, truncation=True, return_tensors='pt')
+#inputs = tokenizer(titles_list, padding=True, truncation=True, return_tensors='pt')
+inputs = tokenizer(articles_list, padding=True, truncation=True, max_length=512, return_tensors='pt')
+
 outputs = model(**inputs)
 
 predictions = torch.nn.functional.softmax(outputs.logits, dim=-1)
@@ -49,7 +57,7 @@ plt.xlabel('Sentiment')
 plt.ylabel('Number of Articles')
 plt.title('Sentiment Analysis of News Titles')
 plt.tight_layout() 
-plt.show()
+#plt.show()
 
 wandb.init(project="FinBERT_Sentiment_Analysis")
 wandb.run.log({"Financial News Sentiment Analysis" : wandb.Table(dataframe=df)})
